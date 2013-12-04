20f498430187fd92ff3947a983b3e9fbfc93a554
VivianLi <lihongwei1004@163.com>
2013-12-04 12:08:07 +0800

    update HomeCell
    update HomeCell

--- Poker/View/HallView/CustomView/HomeCell.m
+++ Poker/View/HallView/CustomView/HomeCell.m
@@ -15,20 +15,20 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         self.userInteractionEnabled = YES;
-        
+        //玩家id
         // Initialization code
         _idLabel = [[UILabel alloc]initWithFrame:CGRectMake(5, 10, 20, 20)];
         _idLabel.text = @"ID:";
         _idLabel.font = [UIFont boldSystemFontOfSize:12];
         _idLabel.textColor = [UIColor whiteColor];
         [self addSubview:_idLabel];
-        
+        //玩家id号
         _playID = [[UILabel alloc]initWithFrame:CGRectMake(_idLabel.frame.size.width+_idLabel.frame.origin.x, 10, 80, 20)];
         _playID.text = @"34242";
         _playID.textColor = [UIColor whiteColor];
         _playID.font = [UIFont boldSystemFontOfSize:12];
         [self addSubview:_playID];
-        
+        //玩家数量
         _playNameLabel = [[UILabel alloc]initWithFrame:CGRectMake(5, _playID.frame.size.height+_playID.frame.origin.y-3, 35, 20)];
         _playNameLabel.textColor = [UIColor whiteColor];
         _playNameLabel.text = @"玩家:";
@@ -40,7 +40,7 @@
         _playNum.textColor = [UIColor whiteColor];
         _playNum.font = [UIFont boldSystemFontOfSize:12];
         [self addSubview:_playNum];
-        
+        //小大盲
         _BigSmallLabel = [[UILabel alloc]initWithFrame:CGRectMake(_playNum.frame.size.width+_playNum.frame.origin.x+35, 27, 40, 20)];
         _BigSmallLabel.textColor = [UIColor whiteColor];
         _BigSmallLabel.font = [UIFont boldSystemFontOfSize:12];
@@ -52,7 +52,7 @@
         _BigSmallNumber.textColor = [UIColor whiteColor];
         _BigSmallNumber.font = [UIFont boldSystemFontOfSize:12];
         [self addSubview:_BigSmallNumber];
-        
+        //买入
         _buyLabel = [[UILabel alloc]initWithFrame:CGRectMake(_BigSmallNumber.frame.size.width+_BigSmallNumber.frame.origin.x+10, 27, 28, 20)];
         _buyLabel.text = @"买入:";
         _buyLabel.font = [UIFont boldSystemFontOfSize:12];
